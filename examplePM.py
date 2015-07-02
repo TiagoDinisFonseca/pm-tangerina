e649191a28fb01c9d61dc0b5bf9ef775e8fa9204
TiagoDinisFonseca <tiago.fonseca@premium-minds.com>
2015-07-02 19:09:21 +0100

    Optimize esta separado

--- examplePM.py
+++ examplePM.py
@@ -4,14 +4,11 @@
 import csv
 import math
 from termcolor import colored
-import scipy.special as special
-from scipy.optimize import minimize_scalar
-from scipy.optimize import minimize
 
+import optimization as O
 import clustering as Cl
 
 DEFAULT_ERROR = 0.4
-MINIMUM_LOG = -20
 
 class Main():
 
@@ -30,7 +27,7 @@ class Main():
 
 		score = self.computeProbabilities()
 		for d in score:
-			o = Optimization(self.filterData(d), score[d])
+			o = O.Optimization(self.filterData(d), score[d])
 			e = o.getInitialLikelyhood()
 			if optimize:
 				o.setState(allEquals = True)
@@ -204,185 +201,3 @@ class Main():
 
 		return error / (total * (choices - 1))
 
-class Optimization():
-
-	global answers
-	global grades
-	global names
-	global alpha
-	global allEquals
-	global method
-
-	def __init__(self, answers, scores):
-		self.answers = answers
-		self.transformScores(scores)
-		self.writeBehaviours(scores)
-		self.alpha = 0
-		self.allEquals = False
-		self.method = 'L-BFGS-B'
-
-	def setState(self, method = 'L-BFGS-B', allEquals = False, alpha = 0):
-		self.method = method
-		self.allEquals = allEquals
-		self.alpha = alpha
-
-	def getInitialLikelyhood(self):
-		x = self.getInitialState()
-		return self.getLikelyhood(x)
-
-	def writeBehaviours(self, scores):
-		self.names = list()
-		for d in scores:
-			self.names.append(d['name'])
-
-	def transformScores(self, scores):
-		self.grades = dict()
-		for score in scores:
-			self.grades[score['name']] = {'w': score['w'], 'e': score['e']}
-
-	def getInitialState(self):
-		first = self.names[0]
-
-		x = list()
-		if self.allEquals:
-			x.append(self.grades[first]['e'])
-
-		for name in self.names:
-			if name != first:
-				x.append(self.grades[name]['w'] - self.grades[first]['w'])
-				if not self.allEquals:
-					x.append(self.grades[name]['e'])
-		return x
-
-	def transformXinGrades(self, x):
-
-		first = self.names[0]
-		grades = dict()
-
-		if self.allEquals:
-			grades[first] = {'w': self.grades[first]['w'], 'e': x[0]}
-			position = 1
-		else:
-			grades[first] = {'w': self.grades[first]['w'], 'e': self.grades[first]['e']}
-			position = 0
-		
-		for name in self.names:
-			if name != first:
-				if self.allEquals:
-					grades[name] = {'w': x[position] + self.grades[first]['w'], 'e': x[0]}
-					position += 1
-				else:
-					grades[name] = {'w': x[position] + self.grades[first]['w'], 'e': x[position + 1]}
-					position += 2
-
-		return grades
-
-	def transformXinScores(self, x):
-
-		grades = self.transformXinGrades(x)
-		scores = list()
-		for d in grades:
-			scores.append({'name': d, 'w': grades[d]['w'], 'e': grades[d]['e']})
-		return scores
-
-	def getBoundaries(self):
-
-		n = len(self.names)
-		b = list()
-
-		if self.allEquals:
-			b.append((0.2, 0.8))
-		for i in range(n-1):
-			b.append((-1, 1))
-			if not self.allEquals:
-				b.append((0.2, 0.8))
-
-		return b
-
-	def dressing(self, x):
-
-		tmp = self.transformXinGrades(x)
-		diff = 0
-		count = 0
-		for i in tmp:
-			for j in tmp:
-				if i != j:
-					count += 1
-					diff += (tmp[i]['e'] - tmp[j]['e'])**2
-
-		return self.alpha * diff
-
-	def minimizeLikelyhood(self, verbose = False, cl_max = 0.5):
-
-		r = dict()
-		x = self.getInitialState()
-		r['state_i'] = self.transformXinScores(x)
-		r['v_i'] = self.getLikelyhood(x)
-		r['dressed_i'] = self.getDressedLikelyhood(x)
-
-		if(verbose):
-			print colored("\tInitial: %.1f" % r['v_i'], "red")
-			clustering = Cl.Clustering(r['state_i'])
-			clustering.getClustering(cl_max, verror = True)
-
-		b = self.getBoundaries()
-		# L-BFGS-B, TNC, COBYLA, SLSQP
-		result = minimize(self.getDressedLikelyhood, x, bounds = b, method = self.method)
-
-		r['state_f'] = self.transformXinScores(result.x)
-		r['v_f'] = self.getLikelyhood(result.x)
-		r['dressed_f'] = self.getDressedLikelyhood(result.x)
-
-		if verbose:
-			print colored("\tFinal: %.1f" % result.fun, "red")
-			clustering = Cl.Clustering(self.transformXinScores(result.x))
-			clustering.getClustering(cl_max, verror = True)
-
-		return r
-
-	def getDressedLikelyhood(self, x):
-		return self.dressing(x) + self.getLikelyhood(x)
-
-	def getLikelyhood(self, x):
-
-		tmp = self.transformXinGrades(x)
-
-		likelyhood = 0
-		for dilemma in self.answers:
-			likelyhood -= self.getIndividualLikelyHood(dilemma['dilemma'], tmp)
-
-		return likelyhood
-
-	def getIndividualLikelyHood(self, dilemma, tmp):
-
-		prob = dict()
-		for b in dilemma:
-			partial = 1
-			for x in dilemma:
-				if x != b:
-					partial *= self.probabilityOneAgainstOne(tmp[b]['w'] - tmp[x]['w'], tmp[b]['e']**2 + tmp[x]['e']**2)
-			prob[b] = partial
-
-		total = sum(prob.values())
-		for p in prob:
-			prob[p] = prob[p] / total
-
-		tmp = self.getLikelyhoodFromProbabilities(dilemma, prob)
-		if tmp <= 0:
-			return MINIMUM_LOG
-		else:
-			return math.log(tmp)
-
-	def getLikelyhoodFromProbabilities(self, dilemma, prob):
-
-		n = sum(dilemma.values())
-		product = math.factorial(n)
-		for b in prob:
-			if b in dilemma:
-				product *= math.pow(prob[b], dilemma[b])
-		for b in dilemma:
-			product *= float(1) / math.factorial(dilemma[b])
-		return product
-
-	def probabilityOneAgainstOne(self, difference, variance):
-		return 0.5 + 0.5 * special.erf(difference / math.sqrt(2 * variance))
--- read_files.py
+++ read_files.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import csv
+from termcolor import colored
 
 class read_file():
 
@@ -12,7 +13,7 @@ class read_file():
 		self.fileIn = fileIn
 		self.fileOut = fileOut
 
-	def read_file(self):
+	def main(self):
 		f = open(self.fileIn, 'r')
 		data = csv.reader(f, delimiter = ',', quotechar = '"')
 		header = data.next()
@@ -36,14 +37,30 @@ class read_file():
 				dimensions.append(tmp)
 				questions.append(header[i])
 
+		failed = 0
+
+		guys = list()
 		for row in data:
+			guy = list()
 			for i in range(qMin, qMax + 1):
 				tmp = self.identifyAnswer(row[i])
 				if tmp == None:
-					print row[i]
+					print colored("Erro: string não reconhecida: ", "red"), row[i]
+					return
+				else: 
+					guy.append(tmp)
+			guys.append(guy)
 
+		g = open(self.fileOut, 'w')
+		output = csv.writer(g, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_NONNUMERIC)
+
+		output.writerow(dimensions)
+		output.writerow(questions)
+		for guy in guys:
+			output.writerow(guy)
 	
-
+		f.close()
+		g.close()
 	def identifyQuestion(self, frase):
 
 		questions = dict()
@@ -77,7 +94,7 @@ class read_file():
 		questions[D3].append("After delivering a work to a client, you realize that you messed up. Would you:")
 
 
-		D4 = 'Tomada de desição'
+		D4 = 'Tomada de decisão'
 		questions[D4] = list()
 		questions[D4].append("You find an error that for the moment does not seem to be a problem. Would you:")
 		questions[D4].append("You are on a project, and you have arrived at a moment where you will have to make a strategic decision, for which you have the necessary competency and knowledge. Would you:")
@@ -91,4 +108,129 @@ class read_file():
 		return None
 
 	def identifyAnswer(self, frase):
-		return 0	
+
+		answers = dict()
+		# Comunicação
+		B00 = 'Tolerância'
+		answers[B00] = list()
+		answers[B00].append("Try to manage the reaction of the team facing the error, accepting the various sentiments expressed by the different members?")
+		answers[B00].append("Accept the supplier's situation and try to renegotiate the deadline with the client?")
+		B01 = 'Empatia'
+		answers[B01] = list()
+		answers[B01].append("Schedule a meeting with the supplier so you can better understand the situation, and together find a solution?")
+		answers[B01].append("Try to look at the problem from a new perspective, because you know that a new member can bring new findings?")
+		answers[B01].append("Try to understand why the person needs to speak to you?")
+		B02 = 'Assertividade'
+		answers[B02] = list()
+		answers[B02].append("Explain that you need silence to concentrate, and thank him/her for understanding your needs?")
+		answers[B02].append("Say that the solution is not good, and guide him/her with the experience you have in the project?")
+		answers[B02].append("Explain your commitment assertively, and give a deadline to the supplier to present a viable solution?")
+		B03 = 'Objetividade'
+		answers[B03] = list()
+		answers[B03].append("Try to explain your point of view simply and clearly?")
+		answers[B03].append("Ask him to stop talking and to concentrate on his/her work?")
+		B04 = 'Escuta ativa'
+		answers[B04] = list()
+		answers[B04].append("Try to better understand the solution he/she proposed, clarifying the details, in order to form a consistent opinion?")
+		answers[B04].append("Try to structure what is discussed in the meeting, in order to present a clear summary?")
+
+		# Relacionamento interpessoal
+		B10 = 'Ser apoiante'
+		answers[B10] = list()
+		answers[B10].append("Offer to help the new member when necessary?")
+		answers[B10].append("Propose to get someone on the task who can help?")
+		B11 = 'Colaboração'
+		answers[B11] = list()
+		answers[B11].append("Offer yourself to look for a restaurant that is good for everyone to talk freely?")
+		answers[B11].append("Join the other team and help solve problems?")
+		answers[B11].append("Try to do tasks together with the new member?")
+		B12 = 'Liderança'
+		answers[B12] = list()
+		answers[B12].append("Challenge your team to collaborate with the other team to overcome the situation?")
+		answers[B12].append("Motivate the person who is not comfortable, and remind him/her of the importance of the team to be all together?")
+		answers[B12].append("Work something out so that the new member receives the optimal task to start?")
+		B13 = 'Partilha aberta de informação'
+		answers[B13] = list()
+		answers[B13].append("Share your solution and all relevant information?")
+		answers[B13].append("Give more information to the person, sharing your experience of what works and what does not work in these types of task?")
+		B14 = 'Orientado para pessoas'
+		answers[B14] = list()
+		answers[B14].append("Propose to the person to begin with a simpler task to gain more experience?")
+		answers[B14].append("Refuse the invitation, and invite the person who was uncomfortable with the suggestion, to have lunch just the two of you?")
+
+		# Recompensa
+		B20 = 'Pagamento mais elevado por bons desempenhos'
+		answers[B20] = list()
+		answers[B20].append("Prefer to receive a monetary bonus?")
+		answers[B20].append("There should be a bonus distributed according to the performance evaluations of each team member.")
+		B21 = 'Equidade'
+		answers[B21] = list()
+		answers[B21].append("The company where salaries are fair and proportional?")
+		answers[B21].append("Ask that you be renewed with a contract under the same terms as people who have similar responsibilities to your own?")
+		answers[B21].append("There should be an equal bonus for all the team members, because everyone contributed to the success of the project.")
+		B22 = 'Segurança no emprego'
+		answers[B22] = list()
+		answers[B22].append("The company should not distribute any bonus, and should use that money to solve their financial problems.")
+		answers[B22].append("Accept to maintain your current job functions, but ask that you receive a permanent contract?")
+		answers[B22].append("The company where there is high job security?")
+		B23 = 'Oportunidade para crescimento pessoal'
+		answers[B23] = list()
+		answers[B23].append("Prefer to be assigned new responsibilities to help you grow professionally?")
+		answers[B23].append("Accept to renew your contract for another six months, but ask that you be given new challenges that allow you to grow within the organization?")
+		B24 = 'Reconhecimento e valorização por bom desempenho'
+		answers[B24] = list()
+		answers[B24].append("Prefer that people you work with recognize your success and value it?")
+		answers[B24].append("The company that recognizes and appreciates people's good performance?")
+
+		# Desempenho
+		B30 = 'Orientado para resultados'
+		answers[B30] = list()
+		answers[B30].append("Try hard to meet the deadline, being available to work overtime?")
+		answers[B30].append("Put all your effort in fulfilling the goals set by the customer?")
+		B31 = 'Criatividade'
+		answers[B31] = list()
+		answers[B31].append("Use time to find a creative solution for the urgent work, at the risk of the work not being adequate?")
+		answers[B31].append("Take advantage to try several different approaches in search of an optimal solution, even though you may not find one?")
+		answers[B31].append("Inform the client, because you know that a blunder is an opportunity for new perspectives?")
+		B32 = 'Gestão de stress'
+		answers[B32] = list()
+		answers[B32].append("Work to identify from the beginning any evidence of problems, in order to protect the team from stress throughout the project?")
+		answers[B32].append("Inform the client and the team, so that the problem is solved as soon as possible?")
+		answers[B32].append("Accept the impossibility of the conflicting priorities, and ask for help to solve the situation?")
+		B33 = 'Elevadas expetativas de desempenho'
+		answers[B33] = list()
+		answers[B33].append("Work hard to ensure that, from day one, everything is run according to best practices, even though this may delay the project?")
+		answers[B33].append("Guide your efforts to develop a work that brings value to the customer?")
+		B34 = 'Competitividade'
+		answers[B34] = list()
+		answers[B34].append("Set yourself to the task so that it exceeds the solutions proposed by the competition?")
+		answers[B34].append("Try to solve the problem without anyone noticing?")
+
+		# Tomada de decisão
+		B40 = 'Autonomia'
+		answers[B40] = list()
+		answers[B40].append("Use some of your time to investigate the origin of the error?")
+		answers[B40].append("Decide according to your experience and your principles, simplifying the decision-making process?")
+		B41 = 'Iniciativa e pragmatismo'
+		answers[B41] = list()
+		answers[B41].append("Ask for him to remain on the project during this difficult phase, and ask him 	to come back to speak about leaving later?")
+		answers[B41].append("Join the team yourself, and work extra hours to ensure on-time delivery?")
+		answers[B41].append("Propose to the team to solve the error quickly?")
+		B42 = 'Adaptabilidade'
+		answers[B42] = list()
+		answers[B42].append("Register and document the error so that the team can decide which is the best strategy to follow?")
+		answers[B42].append("Agree with the person to work half-time on the project, so the team can adapt to the new reality?")
+		answers[B42].append("Focus the team on the problem, requiring extra effort from each member, so 	that you all can adapt to the situation?")
+		B43 = 'Gestão de risco'
+		answers[B43] = list()
+		answers[B43].append("Consult as many people as possible, and from their opinions think about the benefits and risks of each option?")
+		answers[B43].append("Consult the various team members, think about the various opinions, and decide based on that information?")
+		B44 = 'Assumir responsabilidade individual'
+		answers[B44] = list()
+		answers[B44].append("Bring back a former team member, accepting that you will reduce your bonus indexed to the profit margin of the project?")
+		answers[B44].append("Consult the people involved, but it is ultimately up to you to make the  	decision and assume the responsibility?")
+
+		for a in answers:
+			if frase.strip() in answers[a]:
+				return a
+		return None	
